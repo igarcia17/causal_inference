731966d1f1d505311eb3483edc9a91841cfef64e
igarcia17 <ines.garciaortiz99@gmail.com>
2022-01-01 22:30:46 +0100

    uploaded changes to common cause

--- common_cause.R
+++ common_cause.R
@@ -21,4 +21,16 @@ coordinates(comm.cause.DAG) <- list(x = c(Y = 1, X = 2, Z = 3, e_y = 0.75, e_z =
                                     y = c(Y = 1, X = 3, Z = 1, e_y = 0.75, e_z = 0.75))
 drawdag(comm.cause.DAG)
 
+#create dataset in which both X and Y positively increase Z
+set.seed(13)
+rows <- 500
+comm.cause.df1 <- data.frame(X = runif(rows, 1, 100))
+comm.cause.df1$Y <- comm.cause.df1$X * 10 + rnorm(rows)
+comm.cause.df1$Z <- comm.cause.df1$X * 3 + comm.cause.df1$Y * 2 + rnorm(rows)
+
+#create dataset in which X enhances Z, but Y decreases it
+comm.cause.df2 <- data.frame(X = runif(rows, 1, 100))
+comm.cause.df2$Y <- comm.cause.df2$X * 10 + rnorm(rows)
+comm.cause.df2$Z <- comm.cause.df2$X * 3 - comm.cause.df2$Y * 2 + rnorm(rows)
+
 
