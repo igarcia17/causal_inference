9dcbadaed265cbd9021b84b164ed8992ed64be3a
igarcia17 <ines.garciaortiz99@gmail.com>
2022-01-02 11:17:28 +0100

    more advanced comm.cause

--- common_cause.R
+++ common_cause.R
@@ -21,16 +21,32 @@ coordinates(comm.cause.DAG) <- list(x = c(Y = 1, X = 2, Z = 3, e_y = 0.75, e_z =
                                     y = c(Y = 1, X = 3, Z = 1, e_y = 0.75, e_z = 0.75))
 drawdag(comm.cause.DAG)
 
-#create dataset in which both X and Y positively increase Z
+#define the numerical relationship between variables, and size (N)
+N <- 500
+b_xy <- 10
+b_xz <- 3
+b_yz <- 2
+e_x <- 1
+e_y <- 1
+e_z <- 1
+
+#create a dataset in which Z is enhanced by both X and Y
 set.seed(13)
-rows <- 500
-comm.cause.df1 <- data.frame(X = runif(rows, 1, 100))
-comm.cause.df1$Y <- comm.cause.df1$X * 10 + rnorm(rows)
-comm.cause.df1$Z <- comm.cause.df1$X * 3 + comm.cause.df1$Y * 2 + rnorm(rows)
+comm.cause.df1 <- data.frame(X = runif(N, 1, 100) + rnorm(N, sd = e_x))
+comm.cause.df1$Y <- comm.cause.df1$X * b_xy + rnorm(N, sd = e_y)
+comm.cause.df1$Z <- comm.cause.df1$X * b_xz + comm.cause.df1$Y * b_yz + rnorm(N, sd = e_z)
 
 #create dataset in which X enhances Z, but Y decreases it
-comm.cause.df2 <- data.frame(X = runif(rows, 1, 100))
-comm.cause.df2$Y <- comm.cause.df2$X * 10 + rnorm(rows)
-comm.cause.df2$Z <- comm.cause.df2$X * 3 - comm.cause.df2$Y * 2 + rnorm(rows)
+comm.cause.df2 <- data.frame(X = runif(N, 1, 100) + rnorm(N, sd = e_x))
+comm.cause.df2$Y <- comm.cause.df2$X * b_xy + rnorm(N, sd = e_y)
+comm.cause.df2$Z <- comm.cause.df2$X * b_xz - comm.cause.df2$Y * b_yz + rnorm(N, sd = e_z)
+
+#how does each of the scenarios behave in the adjustment of Y?
+
+summary(lm(Z~X+Y, data = comm.cause.df1))
+summary(lm(Z~X, data = comm.cause.df1))
+
+summary(lm(Z~X+Y, data = comm.cause.df2))
+summary(lm(Z~X, data = comm.cause.df2))
 
 
