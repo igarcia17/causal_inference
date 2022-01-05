20154fba21144c23d4f2b7280d9d169f278257c7
igarcia17 <ines.garciaortiz99@gmail.com>
2022-01-06 00:46:11 +0100

    started scenario 2

--- common_cause.R
+++ common_cause.R
@@ -53,19 +53,6 @@ create.dataset <- function(b_yz, N = 500, b_xy = 3, b_xz = 3,
   return(name_df)
 }
 
-Ynoinfluences <- create.dataset(0)
-Yinfluences <- create.dataset(-2)
-non.influences <- create.dataset(0, b_xz = 0)
-#wrongcommoncause <- create.dataset(5, b_xz = 0)
-
-#there are three possibilities to analyze the causal relations
-summary(lm(Z~X+Y, data = Ynoinfluences)) #only X is significant
-summary(lm(Z~X, data = Ynoinfluences)) #significant, total effect of X
-summary(lm(Z~Y, data = Ynoinfluences)) #significant
-
-summary(lm(Z~X+Y, data = Yinfluences)) #both significant, only direct effect of Z
-summary(lm(Z~X, data = Yinfluences)) #significant, total effect of X
-summary(lm(Z~Y, data = Yinfluences)) #significant
 
 #This function will be handy later
 create.datasetv2 <- function(b_ax, b_yz=0, N = 500, b_xy = 3, b_xz = 3,
@@ -76,8 +63,6 @@ create.datasetv2 <- function(b_ax, b_yz=0, N = 500, b_xy = 3, b_xz = 3,
   name_df$Z <- name_df$X * b_xz + name_df$Y * b_yz + rnorm(N, sd = e_z)
   return(name_df)
 }
-
-
 
 #We create a function that checks which is the scenario. We assume that X is in any
 #case a common cause of both Y and Z
@@ -123,8 +108,8 @@ c <- Y_check(Ynoinfluences)
 
 #We will now create a toy example to illustrate the problems of a bad modeling of 
 #scenario 1.
-#We want to study the expression of gene INK4a, key for melanoma development. It will be
-#the outcome variable or Z.
+#We will study the expression of gene INK4a, key for melanoma development. It will be
+#the outcome variable of Z.
 #It is directly affected by UV radiation. UV radiation can come from sunbathing, which
 #increases the appetite for ice cream consumption (measured in ml of consumed ice cream)
 #You collect data of potentially cancerous tissue from 100 people, from which you know the
@@ -171,12 +156,13 @@ sc1.comm <- function(b_yz, N, b_xz, b_xy, reps = 100, ...){
   cat('\nWhen Z ~ Y: \nThe p value of Y is ', mean(onlyY_pv),'\n')
   cat('\nWhen Z ~Y + X: \nThe p value of Y is ', mean(both_pv), '\n')
   
-  cat('\n Change in effect of X over Z\n')
-  cat('Input x -> z: ', b_xz,'\n')
+  cat('\n Change in effect of X over Z')
+
   cat('\nWhen Z ~ X: \nThe estimate for X is ', mean(onlyX_coefX),'and its s.d. is',
       sd(onlyX_coefX),'\n')
   cat('\nWhen Z ~Y + X: \nThe estimate for X is ', mean(both_coefX), 
       'and its s.d. is', sd(both_coefX),'\n')
+  cat('\nBeing input x -> z: ', b_xz)
   #This illustrates how, even if the estimate of the coefficient for X is similar in both cases, the variance is higher in the presence of Y
   op <- par(mfrow= c(2,1), mar = rep(3,4))
   hist(onlyX_coefX, main = 'Z ~ X', xlab = 'Effect X over Z')
@@ -184,6 +170,10 @@ sc1.comm <- function(b_yz, N, b_xz, b_xy, reps = 100, ...){
   hist(both_coefX, main = 'Z ~ X + Y', xlab = 'Effect X over Z')
   abline(v = b_xz, col = 'red')
   par(op)
+  
+  list <- list('onlyY_pv' = onlyY_pv, 'both_pv' = both_pv, 
+               'onlyX_coefX' = onlyX_coefX, 'both_coefX' = both_coefX)
+  invisible(list)
   }
 
 sc1.comm(b_yz = b_yz_i_uv_i, N = samplesize, b_xz = b_xz_i_uv_i, b_xy = b_xy_i_uv_i)
@@ -217,7 +207,7 @@ b_yz_i_uv_i2 <- 0
 #it will be skipped in this one. We are interested in knowing if the sun, A, plays a role 
 #in the value of INK4a, Z, and how important is it.
 
-sc1.comm.plusancestor <- function(b_yz, N, b_xz, b_xy, b_ax, reps = 30, ...){
+sc1.comm.plusancestor <- function(b_yz, N, b_xz, b_xy, b_ax, reps = 30, e_x= 1, ...){
   onlyA_pvA <- rep(NA, reps)
   bothXA_pvA <- rep(NA,reps)
   three_pvA <- rep(NA, reps)
@@ -234,10 +224,11 @@ sc1.comm.plusancestor <- function(b_yz, N, b_xz, b_xy, b_ax, reps = 30, ...){
   bothXA_pvX <- rep(NA, reps)
   three_pvX <- rep(NA, reps)
   
+  #set.seed(13) #can be uncommented for reproducibility
   for (i in 1:reps) {
     
     dataset <- create.datasetv2(b_yz= b_yz, N = N, b_xz = b_xz, b_ax = b_ax,
-                                b_xy = b_xy, ...)
+                                b_xy = b_xy, e_x =e_x, ...)
     three <- lm(Z~X+A+Y, data = dataset)
     bothXA <- lm(Z~X+A, data = dataset)
     onlyA <- lm(Z~A, data = dataset)
@@ -288,7 +279,6 @@ sc1.comm.plusancestor <- function(b_yz, N, b_xz, b_xy, b_ax, reps = 30, ...){
   abline(v = b_xz*b_ax, col = 'red')
   hist(three_coefA, main='Z ~X + A + Y', xlab = 'Effect A over Z')
   abline(v = b_xz*b_ax, col = 'red')
-  par(op)
   
   ###Changes in X
   #p value
@@ -309,7 +299,6 @@ sc1.comm.plusancestor <- function(b_yz, N, b_xz, b_xy, b_ax, reps = 30, ...){
   
   #p valor de X en los modelos, en histograma
   
-  op <- par(mfrow= c(2,3), mar = rep(2,4))
   hist(onlyX_pvX, main = 'Z ~ X', xlab = 'p value of X')
   hist(bothXA_pvX, main = 'Z ~ X + A', xlab = 'p value of X')
   hist(three_pvX, main='Z ~X + A + Y', xlab = 'p value of X')
@@ -320,6 +309,7 @@ sc1.comm.plusancestor <- function(b_yz, N, b_xz, b_xy, b_ax, reps = 30, ...){
   abline(v = b_xz, col = 'red')
   hist(three_coefX, main='Z ~X + A + Y', xlab = 'Effect X over Z')
   abline(v = b_xz, col = 'red')
+  
   par(op)
 }
 
@@ -336,17 +326,125 @@ impliedConditionalIndependencies(i_uv_i_sun.DAG)
 #increases, though its p value is never below any sensible significance level by adjusting by other
 #covariates.
 
-
-#But when the effect of A over X is greater, this changes:
-
+#As seen in the cause of the cause previous work from Ramón Díaz Uriarte, when the standard
+#error of X increases the variance of the estimate doesn't change as much with the 
+#presence of A on the model. We are still working on why this happens.
 sc1.comm.plusancestor(b_yz = b_yz_i_uv_i2, N = samplesize, b_xz=b_xz_i_uv_i2,
-                      b_ax = 100)
+                      b_ax = b_ax_i_uv_i2, b_xy=b_xy_i_uv_i2, e_x =10)
 
+#This function also illustrates a key property of causal inference: same rules for
+#simple models (toy example in Z_X_Y_adjust.R) can be applied to more complex models
+#(as in this case).
+
+###
+
+#Let's move on to the scenario 2. We will illustrate with another example what to expect
+#conditioning on the different possibilities. We have concluded that INK4a over expression
+#is caused by UV radiation. A recent study shows that there is also intervention of 
+#MATP in the French population in this process. It seems to follow the following
+#DAG.
+m_uv_i.DAG <- dagitty("dag {
+UV.radiation -> MATP
+UV.radiation -> INK4a
+MATP -> INK4a
+}")
+
+#coordinates(i_uv_i_sun.DAG) <- list(x = c(Y = 1, X = 2, Z = 3),
+#y = c(Y = 3, X = 1, Z = 3))
+drawdag(m_uv_i.DAG)
+
+samplesize <- 100
+b_xz_m_uv_i <- 4
+b_yz_m_uv_i <- (-3)
+b_xy_m_uv_i <- 2
+
+
+sc2.comm <- function(b_xz, b_yz, b_xy, N, reps = 50, ...) {
+  #for simplicity, p value is not checked as in any model the covariates are causally related
+  onlyX_coefX <- rep(NA,reps)
+  both_coefX <- rep(NA, reps)
+  onlyY_coefY <- rep(NA,reps)
+  both_coefY <- rep(NA,reps)
+  
+  for (i in 1:reps){
+    dataset <- create.dataset(N=N, b_xz = b_xz, b_yz = b_yz, b_xy = b_xy)
+    both <- lm(Z~X + Y, dataset)
+    onlyY <- lm(Z~Y, dataset)
+    onlyX <- lm(Z~X, dataset)
+    
+    onlyX_coefX[i] <- summary(onlyX)$coefficients['X', 'Estimate']
+    both_coefX[i] <- summary(both)$coefficients['X', 'Estimate']
+    onlyY_coefY[i] <- summary(onlyY)$coefficients['Y', 'Estimate']
+    both_coefY[i] <- summary(both)$coefficients['Y', 'Estimate']
+    
+    rm(dataset)
+  }
+  
+  cat('___Changes in X\n')
+  cat('When Z ~ X:\nX coefficient:', mean(onlyX_coefX), 's.d:', sd(onlyX_coefX))
+  cat('\nWhen Z ~ X + Y:\nX coefficient:', mean(both_coefX), 's.d:', sd(both_coefX))
+  
+  cat('\n\n___Changes in Y\n')
+  cat('When Z ~ Y:\nY coefficient:', mean(onlyY_coefY), 's.d:', sd(onlyY_coefY))
+  cat('\nWhen Z ~ Y + X:\nY coefficient:', mean(both_coefY), 's.d:', sd(both_coefY))
+  ###histogramas y revisar los efectos directos e indirectos
+ }
+
+sc2.comm(b_xz = b_xz_m_uv_i, b_yz = b_yz_m_uv_i, b_xy = b_xy_m_uv_i,
+         N = samplesize)
+#In this case, Y, thus, MATP, has a significant p value in both models, in presence
+#and absence of the common cause X, UV radiation.
+
+#The study goes on and we discovers that both the expression of MATP and INK4a
+#is also influenced by another key factor, which is the cortisol level of the 
+#individual. This leaves us with the following DAG:
+
+m_uv_i_c.DAG <- dagitty("dag {
+UV.radiation -> MATP
+UV.radiation -> INK4a
+cortisol -> MATP
+cortisol -> INK4a
+MATP -> INK4a
+}")
+
+#coordinates(i_uv_i_sun.DAG) <- list(x = c(Y = 1, X = 2, Z = 3),
+#y = c(Y = 3, X = 1, Z = 3))
+drawdag(m_uv_i_c.DAG)
+
+samplesize <- 100
+b_ay_m_uv_i_c <- 3
+b_az_m_uv_i_c <- 2
+b_xz_m_uv_i_c <- 4
+b_yz_m_uv_i_c <- (-3)
+b_xy_m_uv_i_c <- 2
+
+
+
+###Backdoor criterion
+
+#_____________________________EXPERIMENTS
+
+
+
+
+Ynoinfluences <- create.dataset(0)
+Yinfluences <- create.dataset(-2)
+non.influences <- create.dataset(0, b_xz = 0)
+#wrongcommoncause <- create.dataset(5, b_xz = 0)
+
+#there are three possibilities to analyze the causal relations
+summary(lm(Z~X+Y, data = Ynoinfluences)) #only X is significant
+summary(lm(Z~X, data = Ynoinfluences)) #significant, total effect of X
+summary(lm(Z~Y, data = Ynoinfluences)) #significant
+
+summary(lm(Z~X+Y, data = Yinfluences)) #both significant, x = 3 e = 0.13, y = -2 e = 0.04
+summary(lm(Z~X, data = Yinfluences)) #significant, x = -3 e = 0.003
+summary(lm(Z~Y, data = Yinfluences)) #significant, Y = -1 e = 00
 
 
 set.seed(18)
 i_uv_i2 <- create.datasetv2(b_ax = 2, N = samplesize, b_xy = b_xy_i_uv_i2,
-                             b_xz = b_xz_i_uv_i2, b_yz = b_yz_i_uv_i2)
+                            b_xz = b_xz_i_uv_i2, b_yz = b_yz_i_uv_i2)
 
 onlyX <- lm(Z~X, data = i_uv_i2)
 onlyY <- lm(Z~Y, data = i_uv_i2)
@@ -363,46 +461,17 @@ summary(XandA) #X is significant, A is slightly significant, X= 10 s.d = 0.1, A 
 summary(YandA) #Y is strongly significant, A not at all, depends on the size of A, Y = 1.9 sd = 0.04, A = 0.59 sd = 0.04
 summary(YXA) #X is significant, A is slightly, X = 9.65 err 0.55, A = -0.05 err 0.2, Y = 0.07 err 0.1
 
-set.seed(18)
-i_uv_i2 <- create.datasetv2(b_ax = 200, N = samplesize, b_xy = b_xy_i_uv_i2,b_xz = b_xz_i_uv_i2, b_yz = b_yz_i_uv_i2)
-onlyX <- lm(Z~X, data = i_uv_i2)
-onlyY <- lm(Z~Y, data = i_uv_i2)
-onlyA <- lm(Z~A, data = i_uv_i2)
-YandX <- lm(Z~Y + X, data = i_uv_i2)
-XandA <- lm(Z~X + A, data = i_uv_i2)
-YandA <- lm(Z~Y + A, data = i_uv_i2)
-YXA <- lm(Z~A +Y + X, data = i_uv_i2)
-summary(onlyX) #significant, e = 10, s.e = 0.00
-summary(onlyY) #significant, e = 2, s.e = 0.00
-summary(onlyA) #significant, e = 2000, s.e = 0.03
-#summary(YandX) #X is significant, Y is not; Y = 0.07 sd = 0.1; X = 9.6, s.d= 0.54
-summary(XandA) #X is significant, A is slightly significant, X= 10 s.d = 0.1, A = -6 sd = 20
-summary(YandA) #Y is strongly significant, A not at all, depends on the size of A, Y = 1.9 sd = 0.04, A = 59 sd = 41
-summary(YXA) #X is significant, A is slightly, X = 9.65 err 0.55, A = -6 err 20, Y = 0.07 err 0.1
+impliedConditionalIndependencies(X.is.cause.DAG)
+Y.noin.condboth <- lm(Z~X + Y, data = Ynoinfluences)
+Y.noin.condY <- lm(Z~Y, data = Ynoinfluences)
+Y.noin.condX <- lm(Z~X, data = Ynoinfluences)
+summary(Y.noin.condboth)
+summary(Y.noin.condY)
+summary(Y.noin.condX)
 
 
 #Should we condition for the cause of the common cause?
 impliedConditionalIndependencies(i_uv_i_sun.DAG)
-
-
-#_____________________________
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -473,13 +542,3 @@ S_C_H <- function(N = 100, reps = 1, signlevel = 0.01, b_yz = 0, ... )
   par(op)
 }
 S_C_H()  
-  
-
-
-impliedConditionalIndependencies(X.is.cause.DAG)
-Y.noin.condboth <- lm(Z~X + Y, data = Ynoinfluences)
-Y.noin.condY <- lm(Z~Y, data = Ynoinfluences)
-Y.noin.condX <- lm(Z~X, data = Ynoinfluences)
-summary(Y.noin.condboth)
-summary(Y.noin.condY)
-summary(Y.noin.condX)
