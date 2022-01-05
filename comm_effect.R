6d81075c24aa893351ce0b77195de4fd1f2a5857
Nermina Logo Lendo <95534454+neralogo@users.noreply.github.com>
2022-01-05 18:21:16 +0100

    Working on it; still missing examples

--- comm_effect.R
+++ comm_effect.R
@@ -34,7 +34,7 @@ drawdag(comm.effect.DAG)
 # Let us generate the data according to the DAG:
 
 N <- 500 # Our sample size will be 500
-b_xz <- 3 # 
+b_xz <- 3 # HDHRSHSTRHJTRSJTSRJTDJDJTDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 b_yz <- 2
 sd_z <- 5
 
@@ -44,8 +44,6 @@ X <- runif(N, 1, 5)
 Y <- runif(N, 2, 4)
 Z <- b_xz * X + b_yz * Y + rnorm(N, 0, sd = sd_z)
 
-df_collider_ind <- data.frame(X, Y, Z)
-df_collider_ind
 
 
 # Let's take a look at our data distribution:
@@ -80,20 +78,22 @@ summary(lm(X ~ Y + Z))
 # raise a correlation that wasn't supposed to be there.
 
 
+
+
 #_______________________________________________________________________________
 # We could also have cases in which our X and Y variables have some sort of
 # relation but the estimate changes when we condition on Z.
 
 comm.effect.DAG_2 <- dagitty("dag {
-X2 -> Z2
-Y2 -> Z2
-X2 -> Y2
-e_z -> Z2
+X -> Z
+Y -> Z
+X -> Y
+e_z -> Z
 }")
 
-coordinates(comm.effect.DAG_2) <- list(x = c(X2 = 1, Y2 = 3, Z2 = 2, 
-                                             e_z = 1.25),
-                                       y = c(X2 = 1, Y2 = 1, Z2 = 3, 
+coordinates(comm.effect.DAG_2) <- list(x = c(X = 1, Y = 3, Z = 2, 
+                                             e_z = 1.75),
+                                       y = c(X = 1, Y = 1, Z = 3, 
                                              e_z = 3))
 drawdag(comm.effect.DAG_2)
 
@@ -130,4 +130,96 @@ summary(lm(X2 ~ Y2)) # Positive significant correlation
 summary(lm(X2 ~ Y2 + Z2)) # Negative significant correlation
 
 ## As we can see, in this particular case, conditioning on Z changes the sign 
-##of the estimate for the Y2 variable, as in Simpson's paradox
+## of the estimate for the Y2 variable, as in Simpson's paradox
+
+
+# if the estimate of both 
+
+
+
+#_______________________________________________________________________________
+
+# Collider with ancestor and descendant:
+
+# Imagine we have now a variable (Z) that has a descendant (variable W),
+# does conditioning on any of those two affect the relation between
+# variable X and Y?
+
+# ANCESTOR
+
+comm.effect.DAG_3 <- dagitty("dag {
+X -> Z
+Y -> Z
+W -> Z
+e_z -> Z
+}")
+
+coordinates(comm.effect.DAG_3) <- list(x = c(X = 1, Y = 3, Z = 2, 
+                                             e_z = 1.75, W = 2),
+                                       y = c(X = 1, Y = 1, Z = 2, 
+                                             e_z = 2, W = 3))
+drawdag(comm.effect.DAG_3)
+
+N <- 500 
+b_xz <- 3 
+b_yz <- 2
+b_wz <- 2.5
+sd_z <- 5
+
+set.seed(11)
+
+X3 <- runif(N, 1, 5)
+Y3 <- runif(N, 2, 4)
+W3 <- runif(N, 1.5, 3)
+Z3 <- b_xz * X3 + b_yz * Y3 + W3 * b_wz + rnorm(N, 0, sd = sd_z)
+
+summary(lm(X3 ~ W3)) # No significant correlation found
+summary(lm(X3 ~ W3 + Z3)) # We find a negative correlation between X3 and W3
+summary(lm(X3 ~ Y3 + Z3)) # We find a negative correlation between X3 and Y3
+
+# Therefore, conditioning on Z3 modifies the independence between X3, Y3 and W3.
+
+summary(lm(X3 ~ Y3)) # No significant correlation found
+summary(lm(X3 ~ Y3 + W3)) # No significant correlation found
+
+# But conditioning on W3 does not change the independence between X3 and Y3.
+
+
+
+
+#DESCENDANT
+
+comm.effect.DAG_4 <- dagitty("dag {
+X -> Z
+Y -> Z
+Z -> W
+e_z -> Z
+}")
+
+coordinates(comm.effect.DAG_4) <- list(x = c(X = 1, Y = 3, Z = 2, 
+                                             e_z = 1.75, W = 2),
+                                       y = c(X = 1, Y = 1, Z = 2, 
+                                             e_z = 2, W = 3))
+drawdag(comm.effect.DAG_4)
+
+
+N <- 500 
+b_xz <- 3 
+b_yz <- 2
+b_zw <- 2.5
+sd_z <- 5
+sd_w <- 2
+
+set.seed(11)
+
+X4 <- runif(N, 1, 5)
+Y4 <- runif(N, 2, 4)
+Z4 <- b_xz * X3 + b_yz * Y3 +  rnorm(N, 0, sd = sd_z)
+W4 <- b_zw * Z4 + rnorm(N, 0, sd = sd_w)
+
+summary(lm(X4 ~ Y4)) # No significant correlation found
+summary(lm(X4 ~ Y4 + W4)) # We find a negative correlation between X3 and W3
+summary(lm(X4 ~ Y4 + Z4)) # We find a negative correlation between X3 and W3
+
+# In this case adjusting by Z4 and W4 (its descendant) resulted in a correlation
+# between the variables X4 and Y4. 
